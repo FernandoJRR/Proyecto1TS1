0ceeb4c94e4effd99353bb865904fcf8e077c632
FernandoJRR <fercitotron@gmail.com>
2023-04-14 11:34:56 -0600

    Agregar funcionalidad de boton Azar

--- app-maiz/components/AppHeader.vue
+++ app-maiz/components/AppHeader.vue
@@ -16,10 +16,30 @@
     <v-btn variant="plain" to="/">Portada</v-btn>
     <v-btn variant="plain" to="/secciones">Secciones</v-btn>
     <v-btn variant="plain" to="/acercade">Acerca de</v-btn>
-    <v-btn variant="plain"
+    <v-btn variant="plain" @click="azar"
       >Azar! <img src="/icono-maiz.svg" width="20" />
     </v-btn>
     <v-btn variant="plain" style="margin-left: 53%" to="/admin">Login</v-btn>
     <v-divider />
   </v-container>
 </template>
+<script>
+export default {
+  methods: {
+    azar() {
+      fetch("http://localhost:4500/proyecto-maiz/server-maiz/db_conexion/administracion.php?azar=true").then(
+        response => {
+          let valor = response.json();
+          valor.then(
+            resp=>{
+              console.log(resp);
+              const router = useRouter()
+              router.push("/articulo/"+resp.slug);
+            }
+          );
+        }
+      ).catch();
+    }
+  },
+}
+</script>
